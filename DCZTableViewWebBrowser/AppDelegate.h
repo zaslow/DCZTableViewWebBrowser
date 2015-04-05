31c7d4689ac55c18919d92c9c1ae30fa379921f7
Dean Zaslow <dcz4@pitt.edu>
2015-04-05 14:34:31 -0400

    Initial Commit

---
+++ DCZTableViewWebBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DCZTableViewWebBrowser
+//
+//  Created by Christine Gornall on 4/5/15.
+//  Copyright (c) 2015 Dean Zaslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
