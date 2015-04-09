6258ef735ac87d8f6d771697b52f64259803c818
Zaslow <dcz4@clg62-m125.labs.pitt.edu>
2015-04-09 13:57:10 -0400

    Initial Commit from different computer

--- DCZTableViewWebBrowser/AppDelegate.h
+++ DCZTableViewWebBrowser/AppDelegate.h
@@ -5,6 +5,7 @@
 //  Created by Christine Gornall on 4/5/15.
 //  Copyright (c) 2015 Dean Zaslow. All rights reserved.
 //
+//  Repository successfully fetched from remote GitHub repository
 
 #import <UIKit/UIKit.h>
 
